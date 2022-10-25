f4f2f959da2bcf131647bb986c607e285896d8b3
“Allen Chou <allen.chou@neutec.com.tw>
2022-10-26 04:14:24 +0800

    add routers

---
+++ src/router.ts
@@ -0,0 +1,28 @@
+import { createWebHistory, createRouter } from "vue-router";
+import { RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    alias: "/todo",
+    name: "todo",
+    component: () => import("./components/TodoList.vue")
+  },
+  {
+    path: "/todo/:id",
+    name: "todo-details",
+    component: () => import("./components/Todo.vue")
+  },
+  {
+    path: "/add",
+    name: "add",
+    component: () => import("./components/AddTodo.vue")
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
